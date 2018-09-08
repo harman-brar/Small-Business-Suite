dca551f41bf3937c544f8ac0dfc6883e5c65b582
Harman Brar <hbrar95@gmail.com>
2018-09-07 17:39:09 -0700

    Fulfilled week 1 tasks: Create class with main method that draws on 2 other methods.

---
+++ src/ui/Inventory.java
@@ -0,0 +1,18 @@
+package ui;
+
+public class Inventory {
+
+    public static void countItems() {
+        System.out.print("Counting Items \n");
+    }
+
+    public static void displayItems() {
+        System.out.print("Displaying Items");
+    }
+
+    public static void main(String args[]) {
+        countItems();
+        displayItems();
+    }
+
+}
